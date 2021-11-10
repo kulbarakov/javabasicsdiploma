bea22d4fefcec849850cbefee6085ec553511cf2
Azamat Kulbarakov <mr.kulbarakov@gmail.com>
2021-11-10 10:44:06 +0500

    textColors moved to Class level

--- src/ru/netology/graphics/image/Schema.java
+++ src/ru/netology/graphics/image/Schema.java
@@ -1,9 +1,10 @@
 package ru.netology.graphics.image;
 
 public class Schema implements TextColorSchema {
+    private final char[] textColors = {'#', '$', '@', '%', '*', '+', '-', '\''};
+
     @Override
     public char convert(int color) {
-        char[] textColors = {'#', '$', '@', '%', '*', '+', '-', '\''};
         return textColors[color / 32];
     }
 }
